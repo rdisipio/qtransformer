8756c2669a70fc47ba8bb9dedd39eee42ef3032e
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2021-01-04 15:08:45 -0500

    add base classes

--- train_classifier.py
+++ train_classifier.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     parser.add_argument('-e', '--embed_dim', default=8, type=int)
     parser.add_argument('-s', '--max_seq_len', default=64, type=int)
     parser.add_argument('-f', '--ffn_dim', default=8, type=int)
-    parser.add_argument('-t', '--n_transformer_blocks', default=1, type=1)
+    parser.add_argument('-t', '--n_transformer_blocks', default=1, type=int)
     parser.add_argument('-H', '--n_heads', default=2, type=int)
     parser.add_argument('-q', '--n_qubits', default=0, type=int)
     parser.add_argument('-L', '--n_qlayers', default=1, type=int)
